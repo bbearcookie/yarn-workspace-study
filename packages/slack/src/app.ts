1ac23db199c97bed96345f81d04caf9ee7ace4a9
bbearcookie <aodem@naver.com>
2023-09-22 18:17:23 +0900

    chore: 임시 라우팅 설정

--- packages/slack/src/app.ts
+++ packages/slack/src/app.ts
@@ -1,15 +1,11 @@
 import 'dotenv/config';
 import app from '@/configs/express';
-// import router from '@/routes';
-// import '@/configs/redis';
+import router from '@/routes';
+import '@/configs/redis';
 
 const PORT = process.env.PORT;
 
-// app.use('/', router);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${PORT} port.`);
